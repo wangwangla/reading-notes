abdc1baa86efa8c6af54290ae3149b5886c18577
wangwangla <2818815189@qq.com>
2020-03-19 08:45:06 +0800

    并查集，红黑树

--- algorithm/src/com/algorithm/heap/Heap.java
+++ algorithm/src/com/algorithm/heap/Heap.java
@@ -2,6 +2,22 @@ package com.algorithm.heap;
 
 /**
  * 使用数组来存储，基本并且需要记录结点的个数
+ * 插入数据：
+ *  将数据放在最后一个位置，然后将它进行上浮，然后让他和他的父结点进行比较，如果大于父亲结点，那么就将其上浮一个，直到最后一个结点
+ *  1.将数据插入到最后一个位置
+ *      temp[++N] = t;
+ *      swim(N){
+ *          while(N>1){
+ *              if(temp[N]>temp[N/2]){
+ *                  //交换
+ *              }
+ *              N = N/2;
+ *          }
+ *      }
+ *  2.将结点上浮
+ * 删除数据：
+ *  删除最大的数据，最大数据在跟的地方，将跟与最下方的进行比较
+ *  temp[1] = temp[N-1];
  */
 public class Heap<T extends Comparable<T>>{
     //存储堆中的元素
@@ -9,7 +25,7 @@ public class Heap<T extends Comparable<T>>{
     //记录堆中元素的个数
     private int N;
     public Heap(int capacity){
-        this.temp = (T[])new Object[capacity];
+        this.temp = (T[])new Comparable[capacity];
         this.N = 0;
     }
 
@@ -68,10 +84,12 @@ public class Heap<T extends Comparable<T>>{
 
     public T delMax(){
         //1.将根结点与最后的哪一个节点进行交换
-
+        T old = temp[1];
         //2.将最后一个结点删除
-
+        exch(1,N);
         //3.下沉操作
+        temp[N--] = null;
+        sink(1);
         return null;
     }
 }
